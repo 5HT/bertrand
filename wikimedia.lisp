b4063d6f5b8a6e0ec7c87fda676d56440463087b
Siegmentation <siegmentationfault@yandex.ru>
2020-01-20 20:18:14 +0700

    more examples

--- wikimedia.lisp
+++ wikimedia.lisp
@@ -9,6 +9,8 @@
 
 (axiom ∧-formation α β [α ∧ β])
 (axiom ∧-intro (α true) (β true) ([α ∧ β] true))
+(axiom ∧-elim-left  ([α ∧ β] true) (α true))
+(axiom ∧-elim-right ([α ∧ β] true) (β true))
 
 ;; https://wikimedia.org/api/rest_v1/media/math/render/svg/3a6c22831067960643c6988d6c9889bfe14bed76
 (theorem wikimedia-example
@@ -22,3 +24,10 @@
       (α β
        β [α ∧ β]))
     (β [β → α ∧ β])))
+
+;; https://wikimedia.org/api/rest_v1/media/math/render/svg/87c96716eb4cd6dabc991b253d6d878790f81b6b
+(theorem ∧-rev x ([α ∧ β] true) ([β ∧ α] true)
+  (∧-intro
+    (∧-elim-right (x ()) ())
+    (∧-elim-left  (x ()) ())
+    (α β β α)))
