8175deb671dc7cfcdc0b85a1599ed1bbbbcf793f
Siegmentation <siegmentationfault@yandex.ru>
2020-01-20 16:39:58 +0700

    upload

---
+++ wikimedia.lisp
@@ -0,0 +1,24 @@
+(infix → 25) (infix ∧ 50)
+
+(variables α β)
+
+(axiom truth-elim (α true) α)
+
+(axiom →-intro α (β true) ([α → β] true))
+(axiom mp α [α → β] β)
+
+(axiom ∧-intro α β [α ∧ β])
+(axiom ∧-elim (α true) (β true) ([α ∧ β] true))
+
+;; https://wikimedia.org/api/rest_v1/media/math/render/svg/3a6c22831067960643c6988d6c9889bfe14bed76
+(theorem wikimedia-example
+  u (α true) w (β true)
+  ([α → β → α ∧ β] true)
+  (→-intro
+    (truth-elim (u ()) ())
+    (→-intro
+      (truth-elim (w ()) (α β))
+      (∧-elim (u ()) (w ()) ())
+      (α β
+       β [α ∧ β]))
+    (β [β → α ∧ β])))
