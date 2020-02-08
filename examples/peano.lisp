3f367761a30018baa622ed5ac76671e9e504e3ab
Siegmentation <siegmentationfault@yandex.ru>
2020-02-08 23:29:44 +0700

    1 + 1

--- examples/peano.lisp
+++ examples/peano.lisp
@@ -1,4 +1,24 @@
-(variables x y)
+(variables φ x y z)
+
+(postulate
+  ─────── =-refl
+  (x = x)
+
+  (x = y)
+  ─────── =-symm
+  (y = x)
+
+  (x = y) (y = z)
+  ─────────────── =-trans
+      (x = z)
+
+      (x = y)
+  ─────────────── =-prefix-ind
+  ((φ x) = (φ y))
+
+    (x₁ = y₁) (x₂ = y₂)
+  ─────────────────────── =-infix-ind
+  ((x₁ φ x₂) = (x₂ φ y₂)))
 
 (postulate
   ─────── 0-def
@@ -21,4 +41,26 @@
 
   ─────── 2-def
   (2 nat)
-  (succ-def [x ≔ 1] 1-def))
+  (succ-def [x ≔ 1] 1-def)
+
+         ─────── p
+         (x = y)
+  ───────────────────── =-succ
+  ((succ x) = (succ y))
+  (=-prefix-ind [φ ≔ succ] p))
+
+(postulate
+     (x nat)
+  ───────────── +-zero
+  ((x + 0) = x)
+
+           (x nat) (y nat)
+  ───────────────────────────────── +-succ
+  ((x + (succ y)) = (succ (x + y))))
+
+(theorem
+  ───────────── 1+1
+  ((1 + 1) = 2)
+  (=-trans [x ≔ (1 + 1) y ≔ (succ (1 + 0)) z ≔ 2]
+    (+-succ [x ≔ (succ 0) y ≔ 0] 1-def 0-def)
+    (=-succ [x ≔ (1 + 0) y ≔ 1] (+-zero [x ≔ 1] 1-def))))
